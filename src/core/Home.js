b1a135f159d8f6dcd9448efdfc72eddbf5192c33
suraj1399 <surajkumar068@rediffmail.com>
2020-11-13 04:20:53 +0530

    Intial Commit

---
+++ src/core/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import "../styles.css"
+export default function Home() {
+
+ 
+    return (
+      <div>
+          <h1 className="text-white">Home File </h1>
+      </div>
+    );
+  
+}
